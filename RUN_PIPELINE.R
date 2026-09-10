32383210431b2984428a4cf590b9e15442cd0ef7
Mael-Marquet <mael.marquet3@gmail.com>
2026-09-10 10:35:31 +0200

    Add files via upload

---
+++ RUN_PIPELINE.R
@@ -0,0 +1,14 @@
+##################################################
+# METABOLOMICS WORKFLOW - PIPELINE EXECUTION
+##################################################
+#
+# This is the MAIN ENTRY POINT for running the complete
+# metabolomics analysis pipeline.
+
+# Set working directory to script location
+script_dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
+setwd(script_dir)
+
+source("Main_Script.R")
+
+message("\n✓ Pipeline execution completed !")
